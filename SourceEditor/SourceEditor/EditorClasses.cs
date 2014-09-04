22a0e881df0debd9e8a31817cb8133220b3a312e
Caledrith <john.morgan.nemelka@gmail.com>
2014-09-04 10:31:12 -0600

    Putting files in main folder

---
+++ SourceEditor/SourceEditor/EditorClasses.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SourceEditor
+{
+    public class EditorClasses
+    {
+        [DisplayName("Site Name"),
+        DescriptionAttribute("The name of the site."),
+        CategoryAttribute("Site Configuration")]
+        public string Name { get; set; }
+
+
+        [DisplayName("Friend Code"),
+        DescriptionAttribute("Your 3DS Friend Code. This does not actually have to be a friend code, it's shown in the top left corner of the site. HTML can be used."),
+        CategoryAttribute("Site Information Configuration")]
+        public string LeftLabel { get; set; }
+
+        [DisplayName("Point of Contact"),
+        DescriptionAttribute("Your point of contact. This does not actually have to be a contact link, it's shown in the top right corner of the site. HTML can be used."),
+        CategoryAttribute("Site Information Configuration")]
+        public string RightLabel { get; set; }
+
+
+        [DisplayName("Site Pokémon Links"),
+        DescriptionAttribute("The number of Pokémon displayed at one point in time before loading the next series."),
+        CategoryAttribute("Site Configuration")]
+        public int PaginationCount { get; set; }
+
+        [DisplayName("Pokémon"),
+        DescriptionAttribute("Your available Pokémon. Keep this regularly updated."),
+        CategoryAttribute("Site Pokemon Configuration")]
+        public List<EditorClassesPokemon> Pokemon { get; set; }
+
+        public EditorClasses()
+        {
+            Pokemon = new List<EditorClassesPokemon>();
+        }
+    }
+
+    public class EditorClassesPokemon
+    {
+        public string Species { get; set; }
+        public string Gender { get; set; }
+        public bool Shiny { get; set; }
+        public string Nature { get; set; }
+        public string Ability { get; set; }
+        public string OT { get; set; }
+        public int OTID { get; set; }
+        public string HP { get; set; }
+        public string Atk { get; set; }
+        public string Def { get; set; }
+        public string SpA { get; set; }
+        public string SpD { get; set; }
+        public string Spe { get; set; }
+        public string Move1 { get; set; }
+        public string Move2 { get; set; }
+        public string Move3 { get; set; }
+        public string Move4 { get; set; }
+    }
+}
---
+++ SourceEditor/SourceEditor/Form1.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SourceEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void WriteText(string filePath, string text)
+        {
+            byte[] encodedText = Encoding.Unicode.GetBytes(text);
+
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Create, FileAccess.Write, FileShare.None,
+                bufferSize: 4096, useAsync: true))
+            {
+                sourceStream.Write(encodedText, 0, encodedText.Length);
+            };
+        }
+
+        public static string ReadText(string filePath)
+        {
+            using (FileStream sourceStream = new FileStream(filePath,
+                FileMode.Open, FileAccess.Read, FileShare.Read,
+                bufferSize: 4096, useAsync: true))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buffer = new byte[0x1000];
+                int numRead;
+                while ((numRead = sourceStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    string text = Encoding.Unicode.GetString(buffer, 0, numRead);
+                    sb.Append(text);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public EditorClasses appset = new EditorClasses();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filepath = Application.StartupPath + @"\data.txt";
+            if (File.Exists(filepath))
+            {
+                string n = ReadText(filepath);
+                appset = JsonConvert.DeserializeObject<EditorClasses>(n);
+            }
+
+            Options.SelectedObject = appset;
+        }
+
+        private void Form1_FormClosing(object sender, EventArgs e)
+        {
+            string filepath = Application.StartupPath + @"\data.txt";
+            string json = JsonConvert.SerializeObject(appset, Formatting.Indented);
+            WriteText(filepath, json);
+        }
+    }
+}
